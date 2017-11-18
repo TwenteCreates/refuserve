3f11b034fd8add8a4502e6a5175d3f41e611a760
Mohit Ahuja <ahujamoh@gmail.com>
2017-11-18 23:12:41 +0100

    minor fix parametere wrongly written as string

--- backend/app.py
+++ backend/app.py
@@ -179,7 +179,7 @@ def get_audio_for_youtube_link():
     subprocess.call(['python', 'vtt-to-transcript.py', vtt_fullname, 'transcript',
                      '--scc_lang', lang])
     # 3. tts the transcript
-    file_contents = open('transcript.txt', 'encoding'='utf-8').read()
+    file_contents = open('transcript.txt', encoding='utf-8').read()
     tts = gTTS(text=file_contents, lang='en', slow=True)
     mp3_filename = '%s.mp3' %(filename)
     tts.save(mp3_filename)
