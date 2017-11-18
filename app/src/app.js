8b2ed1b8cd0ef5555839f00483ddecaddddab1d3
Anand Chowdhary <mail@anandchowdhary.com>
2017-11-18 17:01:17 +0100

    AngularJS

--- app/src/app.js
+++ app/src/app.js
@@ -1,1 +1,28 @@
-console.log("hello, world");
+require("angular");
+
+var app = angular.module("hackApp", [require("angular-route")]);
+
+app.config(function($routeProvider) {
+	$routeProvider
+	.when("/", {
+		templateUrl : "views/chat.html"
+	})
+	.when("/red", {
+		templateUrl : "red.htm"
+	})
+	.when("/green", {
+		templateUrl : "green.htm"
+	})
+	.when("/blue", {
+		templateUrl : "blue.htm"
+	});
+});
+
+app.controller("chat", function($scope) {
+	$scope.firstName= "John";
+	$scope.lastName= "Doe";
+});
+
+window.onload = function() {
+	$("#content").css("height", $(document).height() - $(".primary-header").height() - $(".primary-navbar").height() - 5 + "px");
+}
