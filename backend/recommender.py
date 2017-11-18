142a40631713a33fa2b41bfb12883de41fdec976
manesh vaswani <manesh.vaswani@gmail.com>
2017-11-19 00:30:07 +0100

    recommendation and video api logic

---
+++ backend/recommender.py
@@ -0,0 +1,71 @@
+import video_api
+
+
+jobs=[{'title':'Project Management','skills':['PMP certification','Agile Scrum certification','ITIL Foundation certification','CAPM certification','Cisco CCNA','Microsoft Project']},
+      {'title':'Backend Programmer','skills':['PHP','Ruby','Python','Java','.Net','MySQL','Oracle','SQL Server','Zend','Symfony','CakePHP','SVN','CVS','Git']},
+      {'title':'Data Analyst','skills':['Python','R packages','Descriptive and Inferential Statistics','Multivariable Calculus','Linear algebra','Experimental Design','Supervised and Reinforcement Learning','Database Systems','Interpretation of Data','SPSS','Cognos','SAS','MATLAB']},
+      {'title':'Frontend Developer','skills':['HTML/CSS','JavaScript/jQuery','CSS Preprocessing','Version Control Git','Responsive Design','Testing and Debugging','Browser Developer Tools','Building and Automation Tools Web Performance','Command Line']},
+      {'title':'Graphic Designer','skills':['Typography','Adobe Photoshop','Sketch','Adobe InDesign','Quark','HTML and CSS','Photography','Social Media Marketing']}
+    ]
+
+mySkills=[]
+def recommendJobs(userSkills):
+    mySkills=userSkills
+    suggestedJobs=[]
+    matchCount=[]
+    threshold=3
+    for job in jobs:
+        count=0
+        skills=job['skills']
+        for skill in skills:
+            for mySkill in mySkills:
+                if (mySkill == skill):
+                    count+=1
+        if(count>=threshold):   
+            suggestedJobs.append(job['title'])
+        matchCount.append(count)   
+       
+    if(max(matchCount)==0):
+        for job in jobs:
+            suggestedJobs.append(job['title'])
+        
+    elif(len(suggestedJobs)==0):
+         suggestedJobs.append(jobs[matchCount.index(max(matchCount))]['title'])
+           
+    print (matchCount)               
+    print(suggestedJobs)
+    return suggestedJobs
+
+def recommendVideos(userJob):
+    requiredSkills=[]
+    toLearnSkills=[]
+    for job in jobs:
+        if (job['title']== userJob):
+            requiredSkills=job['skills']
+    for skill in requiredSkills:
+        flag=0
+        for mySkill in mySkills:
+            if (skill == mySkill):
+                flag=1
+        if (flag==0):
+            toLearnSkills.append(skill)
+    options=dict()
+    for toLearnSkill in toLearnSkills:        
+        options['term']=toLearnSkill
+        options['part']='id,snippet'
+        options['max_results']=2
+        youtube_search(options)
+        print('\n')
+    print (toLearnSkills)
+    
+    
+    
+mySkills=['Python','HTML/CSS','R packages','JavaScript/jQuery','Multivariable Calculus']
+
+
+
+recommendJobs(mySkills)    
+recommendVideos('Data Analyst')
+
+
+    
---
+++ backend/video_api.py
@@ -0,0 +1,38 @@
+
+import argparse
+
+from googleapiclient.discovery import build
+from keys import DEVELOPER_KEY
+import json
+
+
+YOUTUBE_API_SERVICE_NAME = 'youtube'
+YOUTUBE_API_VERSION = 'v3'
+
+def youtube_search(options):
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+    developerKey=DEVELOPER_KEY)
+    search_response = youtube.search().list(
+        q=options['term'],
+        part=options['part'],
+        maxResults=options['max_results'],
+        videoCaption='closedCaption',
+        type='video'
+        ).execute()
+    
+    listResults=[]
+    vid= dict()
+    
+    for search_result in search_response.get('items', []):
+        if search_result['id']['kind'] == 'youtube#video':
+            vid['title'] =search_result['snippet']['title']
+            vid['id'] =search_result['id']['videoId']
+            vid['thumbnail'] =search_result['snippet']['thumbnails']['default']['url']
+            vid['description'] =search_result['snippet']['description']
+            listResults.append(vid)
+    print (listResults)
+    return(json.dumps(listResults))
+
+
+
+ 
