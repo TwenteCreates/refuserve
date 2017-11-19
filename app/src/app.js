e159fd745a8a5346af132a398b7f0c39399786e5
Anand Chowdhary <mail@anandchowdhary.com>
2017-11-19 03:56:14 +0100

    Add validation and new JS codebase

--- app/src/app.js
+++ app/src/app.js
@@ -1,23 +1,13 @@
 require("angular");
 
+var devMode = 1;
+
 var app = angular.module("hackApp", [require("angular-route"), require("angular-localforage")]);
 
 app.config(function($routeProvider) {
 	$routeProvider
 	.when("/", {
 		templateUrl: "views/chat.html",
-		controller: "chatCtrl"
-	})
-	.when("/red", {
-		templateUrl: "red.htm",
-		controller: "chatCtrl"
-	})
-	.when("/green", {
-		templateUrl: "green.htm",
-		controller: "chatCtrl"
-	})
-	.when("/blue", {
-		templateUrl: "blue.htm",
 		controller: "chatCtrl"
 	});
 });
@@ -30,184 +20,219 @@ app.config(function($routeProvider) {
 // });
 
 app.controller("chatCtrl", function($scope, $timeout, $localForage, $window, $interval) {
-	$localForage.getItem("nErrors").then(function(nErrors) {
-		$scope.nErrors = nErrors ? nErrors : 0;
-	});
-	$scope.selectOption = false;
-	$scope.messages = [];
-	$scope.doAction = function(content) {
-		switch (content.toLowerCase().trim()) {
-			case "/forget":
-				$scope.botSays("Okay, bye bye!").then(function() {
-					$localForage.setItem("messages", null).then(function() {
-						$scope.messages = null;
-						$window.location.reload();
-					});
-				});
-				break;
-			case "/refresh":
-				$scope.botSays("Sounds good, refreshing the app.").then(function() {
-					$window.location.reload();
-				});
-				break;
-			case "/name":
-				$localForage.getItem("name").then(function(name) {
-					$scope.botSays("You told me your name is " + name + ".");
-				});
-				break;
-			default:
-				$scope.botSays(randomFromArray(["👍", "🤔", "🤷"]));
-				break;
-		}
-	}
-	if (!$scope.currentAction) {
-		$localForage.setItem("currentAction", null);
-	}
-	$scope.sendMessage = function() {
-		if ($scope.messageText) {
-			var contentCopy = $scope.messageText;
-			$localForage.getItem("currentAction").then(function(currentAction) {
-				if (currentAction) {
-					var error = 1, errorMessage = "I'm sorry, I didn't quite catch that.";
-					if ($scope.nErrors > 3) {
-						errorMessage = "It seems like you're having trouble with this.";
-					}
-					var content = contentCopy;
-					if (currentAction == "name") {
-						content = content.charAt(0).toUpperCase() + content.slice(1);
-						var botMessage = randomFromArray(["Great! ", "Awesome! ", "Fantastic! ", "That's a pretty name! "]);
-						botMessage += randomFromArray(["Nice to meet you, ", "It's so nice to meet you, ", "Let's be friends, ", "Pleasure to talk to you, ", "Let's get started, "]);
-						botMessage += content + ".";
-						$scope.botSays(botMessage);
-						error = 0;
-					} else if (currentAction == "age") {
-						content = parseInt(content);
-						if (isNaN(content)) {
-							error = 1;
-						} else {
-							var botMessage = randomFromArray(["Great! ", "Awesome! ", "Fantastic! ", "Perfect! "]);
-							botMessage += content + " years young.";
-							$scope.botSays(botMessage);
-							error = 0;
-						}
-					} else if (currentAction == "lang") {
-						var botMessage = sayHelloIn(content) + "! I'll remember this to make things easier for you.";
-						$scope.botSays(botMessage);
-						error = 0;
-					} else if (currentAction == "job") {
-						var botMessage = "That sounds like an exciting job!";
-						$scope.botSays(botMessage);
-						error = 0;
-					} else if (currentAction == "phone") {
-						var botMessage = "Perfect. We'll use this on your resume.";
-						$scope.botSays(botMessage);
-						error = 0;
-					} else if (currentAction == "email") {
-						var botMessage = "No spam, don't worry. We'll only use this to help you find work.";
-						$scope.botSays(botMessage);
-						error = 0;
-					}
-					if (error == 0) {
-						$localForage.setItem("currentAction", null);
-						$scope.currentAction = null;
-						$localForage.setItem(currentAction, content);
-					} else {
-						$scope.nErrors++;
-						$scope.botSays(errorMessage);
-					}
-				} else {
-					$scope.doAction(contentCopy);
+
+	// Initializations
+	$scope.messages = []; // Empty array for messages
+	$scope.inputType = 0; // [0 => text, 1 => select, 2 => range]
+
+	// Array for basic Q and A
+	$scope.questions = [{
+		text: "Hey stranger!"
+	}, {
+		text: "Welcome to RefuServe. What should I call you?",
+		var: "name",
+		reply: "That's a pretty name, {{ var }}!",
+		validate: /^[a-zA-Z ]{2,30}$/
+	}, {
+		text: "I'd love to get to know you better."
+	}, {
+		text: "How old are you? (Enter a number)",
+		var: "age",
+		reply: "Perfect, {{ var }} years young."
+	}, {
+		text: "What's your native language?",
+		var: "lang",
+		options: ["English", "Hindi", "Arabic", "Urdu", "Turkish", "Spanish", "Portuguese", "Russian", "Chinese"],
+		reply: "Beautiful language"
+	}, {
+		text: "Great, I just need some contact information from you now, and we can continue."
+	}, {
+		text: "What's your phone number?",
+		var: "phone",
+		reply: "Perfect. I'll use this on your resume."
+	}, {
+		text: "What's your primary email?",
+		var: "email",
+		validate: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+		reply: "Don't worry, I won't spam you. Maybe the occassional joke with a job reference. 😉"
+	}, {
+		text: "Now comes the exciting part: let's build your work profile!",
+	}, {
+		text: "That's all, folks!"
+	}];
+
+	// Check if history exist in local storage
+	$localForage.getItem("messages").then(function(messages) {
+		if (messages) {
+			$scope.messages = messages;
+			$(".chat-window").animate({ scrollTop: $(".chat-window").prop("scrollHeight") }, 500);
+			$localForage.getItem("QAStatus").then(function(QAStatus) {
+				if (QAStatus != $scope.questions.length + 1) {
+					$scope.converse();
 				}
 			});
-			$scope.messages.push({
-				user: "user",
-				text: $scope.messageText
-			});
-			$scope.messageText = "";
-		}
-		$scope.scrollToBottom();
-		$scope.saveMessages();
-	};
-	$scope.scrollToBottom = function() {
-		$(".chat-window").animate({ scrollTop: $(".chat-window").prop("scrollHeight") }, 500);
-	};
-	$scope.botSays = function(text, action, options) {
-		action = action || null;
-		options = options || null;
-		if (options) {
-			$scope.answerOptions = options;
-			$scope.messageText = "Select an option";
-			$scope.selectOption = 1;
+			devInfo("LocalStorage: Loaded " + messages.length + " messages");
 		} else {
-			$scope.answerOptions = null;
-			$scope.selectOption = 0;
+			$scope.converse();
+			devInfo("LocalStorage: No messages");
 		}
-		console.log(responsiveVoice);
+	});
+
+	// Function to add message to DOM and local storage
+	$scope.addMessage = (user, text) => {
+		$scope.messages.push({
+			user: user,
+			text: text
+		});
+		$localForage.setItem("messages", $scope.messages).then(function() {
+			devInfo("LocalStorage: Saved " + $scope.messages.length + " messages");
+		});
+		$(".chat-window").animate({ scrollTop: $(".chat-window").prop("scrollHeight") }, 500);
+	}
+
+	// Array for debugging methods
+	$scope.debuggers = [{
+		key: "refresh|reload",
+		fn: function() {
+			$window.location.reload();
+		}
+	}, {
+		key: "forget|delete|reset|clear",
+		fn: function() {
+			$localForage.clear().then(function() {
+				$window.location.reload();
+			});
+		}
+	}, {
+		key: "name",
+		fn: function() {
+			$localForage.getItem("name").then(function(name) {
+				if (name) {
+					$scope.say("You told me your name is " + name + ".");
+				} else {
+					$scope.say("I do not know your name yet.");
+				}
+			});
+		}
+	}];
+
+	// Function to handle formSubmit from user
+	var debuggers = [];
+	for (var i = 0; i < $scope.debuggers.length; i++) {
+		var k = $scope.debuggers[i].key.split("|");
+		for (var j = 0; j < k.length; j++) {
+			debuggers.push(k[j]);
+		}
+	}
+	devInfo("Dev commands: " + JSON.stringify(debuggers));
+	$scope.sendMessage = function() {
+		if ($scope.messageText) {
+			var userMessage = $scope.messageText.trim();
+		} else {
+			return;
+		}
+		$scope.messageText = "";
+		$scope.addMessage("user", userMessage);
+		if (debuggers.includes(userMessage.replace("/", "").toLowerCase())) {
+			for (var i = 0; i < $scope.debuggers.length; i++) {
+				if ($scope.debuggers[i].key.includes(userMessage.replace("/", "").toLowerCase())) {
+					$scope.debuggers[i].fn();
+					devInfo("Debugging command " + $scope.debuggers[i].key);
+				}
+			}
+		} else {
+			// Check for reply
+			$localForage.getItem("QAStatus").then(function(QAStatus) {
+				if (QAStatus != $scope.questions.length + 1) {
+					if ($scope.questions[QAStatus - 1]) {
+						if ($scope.questions[QAStatus - 1].var) {
+							if ($scope.questions[QAStatus - 1].validate) {
+								if ($scope.questions[QAStatus - 1].validate.test(userMessage)) {
+									$localForage.setItem($scope.questions[QAStatus - 1].var, userMessage).then(function() {
+										$scope.say($scope.questions[QAStatus - 1].reply.replace("{{ var }}", userMessage.charAt(0).toUpperCase() + userMessage.slice(1))).then($scope.converse);
+									});
+								} else {
+									var responses = ["Hey, I don't think that's a valid input. Sorry if I'm being mean, but my bosses are very strict.", "Hey, come on, I don't think that's your real " + $scope.questions[QAStatus - 1].var + ".", "I don't think that's a valid input. Can you please try again? 🤔", "I'm a bit confused by your response. What do you mean?", "Are you sure that's your " + $scope.questions[QAStatus - 1].var + "? I think you've got it wrong.", "If only " + $scope.questions[QAStatus - 1].var + "s looked like that... come on, your real " + $scope.questions[QAStatus - 1].var + ", please?"];
+									$scope.say(responses[Math.floor(Math.random() * (responses.length - 0 + 1)) + 0]);
+								}
+							} else {
+								$localForage.setItem($scope.questions[QAStatus - 1].var, userMessage).then(function() {
+									$scope.say($scope.questions[QAStatus - 1].reply.replace("{{ var }}", userMessage.charAt(0).toUpperCase() + userMessage.slice(1))).then($scope.converse);
+								});
+							}
+						}
+					}
+				}
+			});
+		}
+	}
+
+	var speed = 100;
+
+	// Function for speaking
+	$scope.say = function(botMessage) {
 		$(".typing-chatbox").show();
 		$timeout(function() {
-			$scope.scrollToBottom();
+			$(".chat-window").animate({ scrollTop: $(".chat-window").prop("scrollHeight") }, 500);
 		}, 1);
 		return new Promise(function(resolve, reject) {
 			$timeout(function() {
 				$(".typing-chatbox").hide();
-				$scope.messages.push({
-					user: "bot",
-					text: text
-				});
-				$scope.scrollToBottom();
-				if (!$scope.currentAction) {
-					$scope.currentAction = action;
-				}
-				$scope.saveMessages();
-				if (!$scope.currentAction) {
-					$timeout(resolve, 100);
-				} else {
-					$interval(function() {
-						if (!$scope.currentAction) {
-							resolve();
-						}
-					}, 100);
-				}
-			}, 100);
+				$scope.addMessage("bot", botMessage);
+				$timeout(resolve, speed);
+			}, speed * 1.5);
 		});
 	}
-	$scope.saveMessages = function() {
-		$localForage.setItem("messages", $scope.messages);
-		$localForage.setItem("currentAction", $scope.currentAction);
-	};
-	$localForage.getItem("messages").then(function(messages) {
-		if (messages) {
-			$scope.messages = messages;
-			$scope.scrollToBottom();
-		} else {
-			$scope.botSays("Hey stranger!").then(function() {
-				$scope.botSays("Welcome to the App. What should I call you?", "name").then(function() {
-					$scope.botSays("I'd love to get to know you better.").then(function() {
-						$scope.botSays("How old are you? (Enter a number)", "age").then(function() {
-							$scope.botSays("What's your native language?", "lang", ["English", "Hindi", "Arabic", "Urdu", "Turkish", "Spanish", "Portuguese", "Russian", "Chinese"]).then(function() {
-								$scope.botSays("Great, I just need some contact information from you now, and we can continue.").then(function() {
-									$scope.botSays("What's your phone number?", "phone").then(function() {
-										$scope.botSays("And what's your email?", "email").then(function() {
-											$scope.botSays("Now comes the exciting part: your work profile!").then(function() {
-												$scope.botSays("What role are you interested in?", "job", ["Frontend Developer", "Backend Developer", "UI/UX Designer", "HR Manager", "Marketing Manager", "Entrepreneur"]).then(function() {
-													$scope.botSays("Where was your last job? Name the company.");
-												});
-											});
-										});
-									});
-								});
-							});
-						});
-					});
-				});
-			});
-		}
-	});
-});
 
-window.onload = function() {
-	// $(".chat-window").css("height", $(document).height() - $(".messenger-block").height() - $(".primary-header").height() - $(".primary-navbar").height() - 5 + "px");
-}
+	// Function to start conversing
+	$scope.converse = function() {
+		var goNext = function(QAStatus) {
+			if ($scope.questions[QAStatus]) {
+				$scope.say($scope.questions[QAStatus].text).then(function() {
+					$localForage.setItem("QAStatus", QAStatus + 1).then($scope.converse);
+				});
+			} else {
+				$scope.inputType = 0;
+				$scope.options = [];
+				$scope.messageText = "";
+			}
+		};
+		$localForage.getItem("QAStatus").then(function(QAStatus) {
+			if (QAStatus <= $scope.questions.length) {
+				var cont = 1;
+				if (!QAStatus) {
+					QAStatus = 0;
+				}
+				if ($scope.questions[QAStatus - 1]) {
+					if ($scope.questions[QAStatus - 1].var) {
+						if ($scope.questions[QAStatus - 1].options) {
+							devInfo("This question has multiple options");
+							$scope.inputType = 1;
+							$scope.messageText = "Select an option";
+							$scope.answerOptions = $scope.questions[QAStatus - 1].options;
+						} else {
+							$scope.inputType = 0;
+							$scope.options = [];
+							$scope.messageText = "";
+						}
+						$localForage.getItem($scope.questions[QAStatus - 1].var).then(function(variable) {
+							if (!variable) {
+								cont = 0;
+							} else {
+								goNext(QAStatus);
+							}
+						});
+					} else {
+						goNext(QAStatus);
+					}
+				} else {
+					goNext(QAStatus);
+				}
+			}
+		});
+	}
+
+});
 
 function randomFromArray(items) {
 	return items[Math.floor(Math.random() * items.length)];
@@ -234,5 +259,11 @@ function sayHelloIn(lang) {
 		default:
 			return "Great";
 			break;
+	}
+}
+
+function devInfo(message) {
+	if (devMode === 1) {
+		console.info(message);
 	}
 }
